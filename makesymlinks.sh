cf5c9c533919a3a754e422f46d7b243e55f60abf
arthur <arthur@milly.(none)>
2012-05-31 15:00:21 +0600

    added fonts symlink

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -10,7 +10,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old            # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="zshrc tmux.conf pentadactylrc conky vimrc.before vimrc.after vimperatorrc"
+files="zshrc tmux.conf pentadactylrc conky vimrc.before vimrc.after vimperatorrc fonts"
 
 ##########
 
