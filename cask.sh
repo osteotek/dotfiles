f72465529f234fcf35cd8374950b2cee2acd15ba
Arthur Tazhitdinov <osteotek@gmail.com>
2016-06-21 14:46:10 +0300

    backup of homebrew

---
+++ cask.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# taps
+brew tap caskroom/cask
+
+# casks
+brew cask install boot2docker-status
+brew cask install cuda-z
+brew cask install julia
+brew cask install qlcolorcode
+brew cask install qlmarkdown
+brew cask install qlprettypatch
+brew cask install qlstephen
+brew cask install quicklook-csv
+brew cask install quicklook-json
+brew cask install vagrant
+brew cask install xquartz
