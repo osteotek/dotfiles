216024753b5c9899e8f1aa9dc3745f1680c5c6a2
Arthur Tazhitdinov <osteotek@gmail.com>
2017-04-15 15:43:53 +0300

    reorganizing files

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old            # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="zshrc tmux.conf vimrc vimperatorrc fonts gitignore gitconfig zsh_favlist"
+files="vimrc vimperatorrc gitignore gitconfig"
 
 ##########
 
